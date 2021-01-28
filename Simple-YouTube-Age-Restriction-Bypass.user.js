27595036069879eeef70be29504486576f52e8eb
zerodytrash <59258980+zerodytrash@users.noreply.github.com>
2021-01-28 23:52:06 +0100

    Refurbished
    Videos are now played automatically through YT's original UI without shortcut and popup!

--- Simple-YouTube-Age-Restriction-Bypass.user.js
+++ Simple-YouTube-Age-Restriction-Bypass.user.js
@@ -1,45 +1,94 @@
 // ==UserScript==
 // @name         Simple YouTube Age Restriction Bypass
 // @namespace    https://zerody.one
-// @version      0.1
-// @description  Press ALT + U to view age restricted YouTube videos :)
+// @version      0.2
+// @description  View age restricted videos on YouTube without verification and login :)
 // @author       ZerodyOne
 // @match        https://www.youtube.com/*
 // @grant        none
+// @run-at       document-body
 // ==/UserScript==
 
 (function() {
-    'use strict';
 
-    document.addEventListener("keydown", function (e) {
-        if (e.altKey && e.key === "u") {
-            var videoId = new URLSearchParams(new URL(document.location).search).get("v");
-            if(!videoId) return;;
+    var nativeParse = window.JSON.parse; // Backup the original parse function to prevent a call loop
+    var useBypassEverywhere = false; // This option can be enabled to block video ads (Adblock), but this can cause negative side effects~
+    var unlockablePlayerStates = ["AGE_VERIFICATION_REQUIRED", "LOGIN_REQUIRED"];
+    var responseCache = {};
 
-            var popup = window.open(null, null, "width=640,height=360,resizable=yes");
-            popup.focus();
-            popup.document.write("<html><body style='color:white;background-color:black;'>Loading video...</body></html>");
+    // Unlock #1: Overwrite PlayerResponse in YT's inital page source
+    if(window.ytInitialPlayerResponse && isUnlockable(window.ytInitialPlayerResponse.playabilityStatus)) {
+        window.ytInitialPlayerResponse = unlockPlayerResponse(window.ytInitialPlayerResponse);
+    }
 
-            fetch("/get_video_info?video_id=" + videoId).then(response => response.text()).then((responseText) => {
-                try {
-                    var playerInfo = JSON.parse(new URLSearchParams(responseText).get("player_response"));
-                    if(!playerInfo.streamingData || !playerInfo.streamingData.formats || !playerInfo.streamingData.formats.length) throw "video file not available.";
+    // Thats Smart! <3
+    window.JSON.parse = function(json) {
+        var parsedData = nativeParse(json);
+        parsedData = inspectJsonData(parsedData);
+        return parsedData;
+    }
 
-                    var videoElement = "<video controls autoplay style='height:100%; width:100%;'>";
-                    playerInfo.streamingData.formats.forEach(function(source){
-                        videoElement += "<source src='" + source.url + "' type='" + source.mimeType + "' />"
-                    });
-                    videoElement += "</video";
+    function inspectJsonData(parsedData) {
+        try {
+            // Unlock #2: Array based in "&pbj=1" AJAX response on any navigation
+            if(Array.isArray(parsedData)) {
+                var playerResponseArrayItem = parsedData.find(e => typeof e.playerResponse === "object");
+                var playerResponse = playerResponseArrayItem ? playerResponseArrayItem.playerResponse : null;
 
-                    popup.document.body.innerHTML = videoElement;
-                    popup.document.title = playerInfo.videoDetails.title;
-
-                } catch(err) {
-                    popup.document.body.innerText = "Error: " + err.toString();
+                if(playerResponse && isUnlockable(playerResponse.playabilityStatus)) {
+                    playerResponseArrayItem.playerResponse = unlockPlayerResponse(playerResponse);
                 }
-            }).catch(err => {
-                popup.document.body.innerText = "Error: " + err.toString();
-            })
+            }
+
+            // Unlock #3: Another JSON-Object
+            if(parsedData.playerResponse && parsedData.playerResponse.playabilityStatus && parsedData.playerResponse.videoDetails) {
+                parsedData.playerResponse = unlockPlayerResponse(parsedData.playerResponse);
+            }
+
+            // Unlock #4: ...and another JSON-Object
+            if(parsedData.playabilityStatus && parsedData.videoDetails && isUnlockable(parsedData.playabilityStatus)) {
+                parsedData = unlockPlayerResponse(parsedData);
+            }
+
+        } catch(err) {
+            console.error("Simple-YouTube-Age-Restriction-Bypass-Error:", err);
         }
-    });
+
+        return parsedData;
+    }
+
+    function isUnlockable(playabilityStatus) {
+        if(!playabilityStatus || !playabilityStatus.status) return false;
+        if(useBypassEverywhere) return true;
+        return unlockablePlayerStates.includes(playabilityStatus.status);
+    }
+
+    function unlockPlayerResponse(playerResponse) {
+        var videoId = playerResponse.videoDetails.videoId;
+        var unlockedPayerResponse = getUnlockedPlayerResponse(videoId);
+
+        // check if the unlocked response isn't playable
+        if(unlockedPayerResponse.playabilityStatus.status !== "OK")
+            throw ("Unlock Failed, playabilityStatus: " + unlockedPayerResponse.playabilityStatus.status);
+
+        return unlockedPayerResponse;
+    }
+
+    function getUnlockedPlayerResponse(videoId) {
+        // Check if is cached
+        if(responseCache.videoId === videoId) return responseCache.content;
+
+        // Query YT's unrestricted api endpoint
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "/get_video_info?video_id=" + videoId, false); // Synchronous!!!
+        xmlhttp.send(null);
+
+        var playerResponse = nativeParse(new URLSearchParams(xmlhttp.responseText).get("player_response"));
+
+        // Cache response for 10 seconds
+        responseCache = { videoId: videoId, content: playerResponse };
+        setTimeout(function() { responseCache = {} }, 10000);
+
+        return playerResponse;
+    }
 })();
