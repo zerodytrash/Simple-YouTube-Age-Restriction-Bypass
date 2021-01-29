cefc69090ec8c28ea47468392c865faa941b71f3
zerodytrash <59258980+zerodytrash@users.noreply.github.com>
2021-01-29 17:19:27 +0100

    Bugfix + Bypass for non-embeddable videos
    ...by using some extra url-params

--- Simple-YouTube-Age-Restriction-Bypass.user.js
+++ Simple-YouTube-Age-Restriction-Bypass.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Simple YouTube Age Restriction Bypass
 // @namespace    https://zerody.one
-// @version      0.2
+// @version      0.3
 // @description  View age restricted videos on YouTube without verification and login :)
 // @author       ZerodyOne
 // @match        https://www.youtube.com/*
@@ -12,7 +12,7 @@
 (function() {
 
     var nativeParse = window.JSON.parse; // Backup the original parse function to prevent a call loop
-    var useBypassEverywhere = false; // This option can be enabled to block video ads (Adblock), but this can cause negative side effects~
+    var useBypassEverywhere = false; // This option can be enabled to block video ads (Adblock), but it can cause negative side effects~
     var unlockablePlayerStates = ["AGE_VERIFICATION_REQUIRED", "LOGIN_REQUIRED"];
     var responseCache = {};
 
@@ -41,7 +41,7 @@
             }
 
             // Unlock #3: Another JSON-Object
-            if(parsedData.playerResponse && parsedData.playerResponse.playabilityStatus && parsedData.playerResponse.videoDetails) {
+            if(parsedData.playerResponse && parsedData.playerResponse.playabilityStatus && parsedData.playerResponse.videoDetails && isUnlockable(parsedData.playerResponse.playabilityStatus)) {
                 parsedData.playerResponse = unlockPlayerResponse(parsedData.playerResponse);
             }
 
@@ -75,15 +75,23 @@
     }
 
     function getUnlockedPlayerResponse(videoId) {
+
         // Check if is cached
         if(responseCache.videoId === videoId) return responseCache.content;
 
         // Query YT's unrestricted api endpoint
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", "/get_video_info?video_id=" + videoId, false); // Synchronous!!!
+        xmlhttp.open("GET", "/get_video_info?video_id=" + encodeURIComponent(videoId), false); // Synchronous!!!
         xmlhttp.send(null);
-
         var playerResponse = nativeParse(new URLSearchParams(xmlhttp.responseText).get("player_response"));
+
+        // If the video is age restricted and the uploader has disallowed the 'Allow embedding' option, these extra params can help in some cases...
+        if (playerResponse.playabilityStatus.status === "UNPLAYABLE") {
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "/get_video_info?video_id=" + encodeURIComponent(videoId) + "&html5=1&eurl&ps=desktop-polymer&el=adunit&cbr=Chrome&cplatform=DESKTOP&break_type=1&autoplay=1&content_v&authuser=0", false); // Synchronous!!!
+            xmlhttp.send(null);
+            playerResponse = nativeParse(new URLSearchParams(xmlhttp.responseText).get("player_response"));
+        }
 
         // Cache response for 10 seconds
         responseCache = { videoId: videoId, content: playerResponse };
