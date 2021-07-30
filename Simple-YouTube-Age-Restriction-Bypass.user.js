ca2740f85b7372197a41d0fdae96098367a2aa74
David <59258980+zerodytrash@users.noreply.github.com>
2021-07-30 23:03:23 +0200

    meta details optimized

--- Simple-YouTube-Age-Restriction-Bypass.user.js
+++ Simple-YouTube-Age-Restriction-Bypass.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name            Simple YouTube Age Restriction Bypass
-// @name:de         Simple YouTube Age Restriction Bypass
-// @version         2.0.6
-// @description     View age restricted videos on YouTube without verification and login :)
+// @description     Watch age restricted videos on YouTube without login and without age verification :)
 // @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen :)
+// @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge :)
+// @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età :)
+// @version         2.0.6
 // @author          Zerody (https://github.com/zerodytrash)
 // @namespace       https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/
 // @updateURL       https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/raw/main/Simple-YouTube-Age-Restriction-Bypass.user.js
@@ -14,6 +15,11 @@
 // @match           https://m.youtube.com/*
 // @grant           none
 // @run-at          document-start
+// @compatible      chrome Chrome + Tampermonkey or Violentmonkey
+// @compatible      firefox Firefox + Tampermonkey or Violentmonkey
+// @compatible      opera Opera + Tampermonkey or Violentmonkey
+// @compatible      edge Edge + Tampermonkey or Violentmonkey
+// @compatible      safari Safari + Tampermonkey or Violentmonkey
 // ==/UserScript==
 
 (function () {
