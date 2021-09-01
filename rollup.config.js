96bac77bf4fa3a7c9807581263728059fa0fbe0c
DrunkWinter <38593134+DrunkWinter@users.noreply.github.com>
2021-09-02 00:54:31 +0200

    Remove `initUnlocker` & wrap with our custom iife
    The reason we now use a custom iife, is because Rollup would not let us use `return`.
    Another reason is to prevent a call to `argument.callee` which
    is deprecated and slow. We now have a named iife which we also provide
    an argument to determine if our function should be promoted
    with `window.eval`.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
+import { wrap } from 'rollup-plugin-insert';
 import { babel } from '@rollup/plugin-babel';
 import userscript from 'rollup-plugin-userscript';
+
+const wrapper = (() => {
+    (function iife(inject) {
+        // Trick to get around the sandbox restrictions in Greasemonkey (Firefox)
+        // Inject code into the main window if criteria match
+        if (typeof GM_info === "object" && GM_info.scriptHandler === "Greasemonkey" && inject) {
+            window.eval("(" + iife.toString() + ")();");
+            return;
+        }
+
+        /* == INJECTION == */
+    })(true);
+}).toString().slice(7, -1).split('/* == INJECTION == */');
 
 export default {
     input: 'src/main.js',
     output: {
         file: 'dist/Simple-YouTube-Age-Restriction-Bypass.user.js',
-        format: 'iife',
-        strict: false,
+        format: 'esm',
     },
     plugins: [
+        // Manually wrap code in our custom iife
+        wrap(...wrapper),
         babel({ babelHelpers: 'bundled' }),
         userscript('userscript.config.js'),
     ],
