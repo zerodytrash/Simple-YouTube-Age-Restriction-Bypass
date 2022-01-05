3875663055d5db7fb2ad3b44451d9b1160a13603
David <59258980+zerodytrash@users.noreply.github.com>
2022-01-05 01:06:29 +0100

    Refactor JSON & XHR interceptor

--- src/components/interceptor.js
+++ src/components/interceptor.js
@@ -30,15 +30,15 @@ export function attachInitialDataInterceptor(onInititalDataSet) {
 
 // Intercept, inspect and modify JSON-based communication to unlock player responses by hijacking the JSON.parse function
 export function attachJsonInterceptor(onJsonDataReceived) {
-    window.JSON.parse = (text, reviver) => {
-        const data = nativeJSONParse(text, reviver);
-        return !isObject(data) ? data : onJsonDataReceived(data);
+    window.JSON.parse = function () {
+        const data = nativeJSONParse.apply(this, arguments);
+        return isObject(data) ? onJsonDataReceived(data) : data;
     };
 }
 
 export function attachXhrOpenInterceptor(onXhrOpenCalled) {
     XMLHttpRequest.prototype.open = function (method, url) {
-        if (arguments.length > 1 && typeof url === 'string' && url.indexOf('https://') === 0) {
+        if (typeof url === 'string' && url.indexOf('https://') === 0) {
             const modifiedUrl = onXhrOpenCalled(this, method, new URL(url));
 
             if (typeof modifiedUrl === 'string') {
