04fda92a8d06537017f13cd0152f6ea962d2a54f
David <59258980+zerodytrash@users.noreply.github.com>
2022-01-13 18:59:20 +0100

    Add manifest v2

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,11 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import html from 'rollup-plugin-html';
 import pkg from './package.json';
-import manifest from './src/extension/manifest.json';
+import manifest from './src/extension/mv3/manifest.json';
+
+const EXTENSION_OUTPUT_DIR = 'dist/extension';
+const EXTENSION_MAIN_SCRIPT_NAME = manifest.content_scripts[0].js[0];
+const EXTENSION_WEB_SCRIPT_NAME = manifest.web_accessible_resources[0].resources[0];
 
 function wrap_in_iife() {
     const [banner, footer] = (() => {
@@ -50,9 +54,16 @@ function copy({ src, dest, transform }) {
     };
 }
 
-const EXTENSION_OUTPUT_DIR = 'dist/extension';
-const EXTENSION_MAIN_SCRIPT_NAME = manifest.content_scripts[0].js[0];
-const EXTENSION_WEB_SCRIPT_NAME = manifest.web_accessible_resources[0].resources[0];
+function getRollupPluginsForManifestVersion(manifestVersion) {
+    const outputDir = EXTENSION_OUTPUT_DIR + '/mv' + manifestVersion;
+    return [
+        copy({ src: `src/extension/mv${manifestVersion}/manifest.json`, dest: outputDir, transform: set_script_version }),
+        copy({ src: 'src/extension/popup.html', dest: outputDir, transform: set_script_version }),
+        copy({ src: 'src/extension/icon/icon_16.png', dest: outputDir }),
+        copy({ src: 'src/extension/icon/icon_48.png', dest: outputDir }),
+        copy({ src: 'src/extension/icon/icon_128.png', dest: outputDir }),
+    ]
+}
 
 export default [
     {
@@ -72,10 +83,16 @@ export default [
     },
     {
         input: 'src/main.js',
-        output: {
-            file: `${EXTENSION_OUTPUT_DIR}/${EXTENSION_WEB_SCRIPT_NAME}`,
-            format: 'iife',
-        },
+        output: [
+            {
+                file: `${EXTENSION_OUTPUT_DIR}/mv2/${EXTENSION_WEB_SCRIPT_NAME}`,
+                format: 'iife',
+            },
+            {
+                file: `${EXTENSION_OUTPUT_DIR}/mv3/${EXTENSION_WEB_SCRIPT_NAME}`,
+                format: 'iife',
+            }
+        ],
         plugins: [
             html(),
             nodeResolve(),
@@ -83,16 +100,19 @@ export default [
     },
     {
         input: 'src/extension/main.js',
-        output: {
-            file: `${EXTENSION_OUTPUT_DIR}/${EXTENSION_MAIN_SCRIPT_NAME}`,
-            format: 'esm',
-        },
+        output: [
+            {
+                file: `${EXTENSION_OUTPUT_DIR}/mv2/${EXTENSION_MAIN_SCRIPT_NAME}`,
+                format: 'esm',
+            },
+            {
+                file: `${EXTENSION_OUTPUT_DIR}/mv3/${EXTENSION_MAIN_SCRIPT_NAME}`,
+                format: 'esm',
+            }
+        ],
         plugins: [
-            copy({ src: 'src/extension/manifest.json', dest: EXTENSION_OUTPUT_DIR, transform: set_script_version }),
-            copy({ src: 'src/extension/popup.html', dest: EXTENSION_OUTPUT_DIR, transform: set_script_version }),
-            copy({ src: 'src/extension/icon/icon_16.png', dest: EXTENSION_OUTPUT_DIR }),
-            copy({ src: 'src/extension/icon/icon_48.png', dest: EXTENSION_OUTPUT_DIR }),
-            copy({ src: 'src/extension/icon/icon_128.png', dest: EXTENSION_OUTPUT_DIR }),
+            ...getRollupPluginsForManifestVersion(2),
+            ...getRollupPluginsForManifestVersion(3),
         ],
     },
 ];
--- scripts/archive-extension.js
+++ scripts/archive-extension.js
@@ -2,14 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import archiver from 'archiver';
 
-const EXTENSION_DIR = 'dist/extension';
+const EXTENSION_DIR_MV2 = 'dist/extension/mv2';
+const EXTENSION_DIR_MV3 = 'dist/extension/mv3';
 const EXTENSION_OUTPUT_FILE_NAME = 'bundle.zip';
 
-const archive = archiver('zip');
+async function archiveExtension(extensionDir, outputFileName) {
+    const archive = archiver('zip');
 
-archive.directory(EXTENSION_DIR, false);
+    archive.directory(extensionDir, false);
+    
+    // wait for archive to finalize before creating a file in the same directory
+    await archive.finalize();
+    
+    archive.pipe(fs.createWriteStream(path.join(extensionDir, outputFileName)));
+}
 
-// wait for archive to finalize before creating a file in the same directory
-await archive.finalize();
-
-archive.pipe(fs.createWriteStream(path.join(EXTENSION_DIR, EXTENSION_OUTPUT_FILE_NAME)));
+await archiveExtension(EXTENSION_DIR_MV2, EXTENSION_OUTPUT_FILE_NAME);
+await archiveExtension(EXTENSION_DIR_MV3, EXTENSION_OUTPUT_FILE_NAME);
