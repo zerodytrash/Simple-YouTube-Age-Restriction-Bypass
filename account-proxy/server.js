bc4c9db20de3d7ba08450788a809bba5830bf30d
David <59258980+zerodytrash@users.noreply.github.com>
2022-01-26 22:50:24 +0100

    Add proxy server monitoring options...
    ...to detect an expired session at an early stage

--- account-proxy/server.js
+++ account-proxy/server.js
@@ -5,11 +5,34 @@ const app = express()
 const rateLimit = require("express-rate-limit")
 const innertube = require("./innertube-api")
 
-app.use(rateLimit({ windowMs: 30000, max: 5 })); // limit requests to 5 per 30s for a single IP
+// limit requests to 5 per 30s for a single IP
+app.use(rateLimit({
+    windowMs: 30000,
+    max: 5,
+    handler: function (req, res) {
+        res.status(429).send({ errorMessage: "Too Many Requests" });
+    },
+}));
+
 app.enable('trust proxy'); // use X-Forwarded-* headers, remove this if you don't use a reverse proxy!
+
+let playabilityHistory = [];
+
+function pushPlayabilityHistory(playabilityStatus) {
+    playabilityHistory.push(playabilityStatus);
+    playabilityHistory = playabilityHistory.slice(-100);
+}
 
 app.get("/", (req, res) => {
     res.redirect("https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass");
+});
+
+app.get("/playabilityHistory", (req, res) => {
+    res.send(playabilityHistory);
+});
+
+app.get("/sessionHealth", (req, res) => {
+    res.send({ loggedIn: !playabilityHistory.includes("LOGIN_REQUIRED") });
 });
 
 app.get("/getPlayer", (req, res) => {
@@ -30,10 +53,13 @@ app.get("/getPlayer", (req, res) => {
             streamingData: videoInfoResponse.data.streamingData,
         }
 
+        pushPlayabilityHistory(publicData.playabilityStatus ? publicData.playabilityStatus.status : "MISSING_STATUS");
+        
         res.status(200).send(publicData);
 
     }).catch(err => {
-        console.error(err);
+        console.error(err.toString());
+        pushPlayabilityHistory("EXCEPTION");
         res.status(500).send({ errorMessage: "proxy backend error: " + err.toString() });
     })
 })
