fbaf1e5e64e32303775d60454a47466918b18978
David <59258980+zerodytrash@users.noreply.github.com>
2022-02-11 20:47:49 +0100

    update zipped extension output filename

--- scripts/archive-extension.js
+++ scripts/archive-extension.js
@@ -4,7 +4,9 @@ import archiver from 'archiver';
 
 const EXTENSION_DIR_MV2 = 'dist/extension/mv2';
 const EXTENSION_DIR_MV3 = 'dist/extension/mv3';
-const EXTENSION_OUTPUT_FILE_NAME = 'bundle.zip';
+const EXTENSION_OUTPUT_FILE_NAME_MV2 = 'extension_manifest_v2_firefox.zip';
+const EXTENSION_OUTPUT_FILE_NAME_MV3 = 'extension_manifest_v3_chromium.zip';
+
 
 async function archiveExtension(extensionDir, outputFileName) {
     const archive = archiver('zip');
@@ -17,5 +19,5 @@ async function archiveExtension(extensionDir, outputFileName) {
     archive.pipe(fs.createWriteStream(path.join(extensionDir, outputFileName)));
 }
 
-await archiveExtension(EXTENSION_DIR_MV2, EXTENSION_OUTPUT_FILE_NAME);
-await archiveExtension(EXTENSION_DIR_MV3, EXTENSION_OUTPUT_FILE_NAME);
+await archiveExtension(EXTENSION_DIR_MV2, EXTENSION_OUTPUT_FILE_NAME_MV2);
+await archiveExtension(EXTENSION_DIR_MV3, EXTENSION_OUTPUT_FILE_NAME_MV3);
