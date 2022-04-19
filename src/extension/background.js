e92c21fcf0656ed0eb4ad74fbbd67d3790545955
David <59258980+zerodytrash@users.noreply.github.com>
2022-04-19 21:59:10 +0200

    add firefox support, switch to active on embed

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,34 +1,17 @@
-const matches = ['.*://www.youtube.com/.*', '.*://m.youtube.com/.*', '.*://music.youtube.com/.*'];
-
-// https://stackoverflow.com/a/64475504
-chrome.declarativeContent.onPageChanged.removeRules(async () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-        {
-            conditions: matches.map(
-                (match) =>
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlMatches: match },
-                    })
-            ),
-            actions: [
-                new chrome.declarativeContent.SetIcon({
-                    imageData: {
-                        16: await loadImageData('icon_16.png'),
-                        48: await loadImageData('icon_48.png'),
-                    },
-                }),
-                chrome.declarativeContent.ShowAction ? new chrome.declarativeContent.ShowAction() : new chrome.declarativeContent.ShowPageAction(),
-            ],
+function setTabIconActive(tabId) {
+    (chrome.browserAction || chrome.action).setIcon({
+        tabId: tabId,
+        path: {
+            16: 'icon_16.png',
+            48: 'icon_48.png',
         },
-    ]);
-});
-
-// SVG icons aren't supported yet
-async function loadImageData(url) {
-    const img = await createImageBitmap(await (await fetch(url)).blob());
-    const { width, height } = img;
-    const canvas = new OffscreenCanvas(width, height);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0, width, height);
-    return ctx.getImageData(0, 0, width, height);
+    });
 }
+
+chrome.runtime.onMessage.addListener((data, sender) => {
+    switch (data.event) {
+        case 'INIT':
+            setTabIconActive(sender.tab.id, true);
+            break;
+    }
+});
