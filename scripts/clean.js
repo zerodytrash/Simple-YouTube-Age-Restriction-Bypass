36da83d9c4ae82098939e0c3780dde5db68e5c16
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2022-10-12 15:36:16 +0200

    [chore] switch to a build script
    This is a more extensible build system and improves performance significantly.
    
    By executing `clean` and `archive-extension` in the same JS context, startup cost of two additional contexts is eliminated.
    Benchmark results show an improvement of ~2.7s. Down from ~6.7s to ~4.0s after this patch.
    
    Also by moving away from a config/plugin approach we can easily reorder or insert build steps without too much hassle.
    
    This also lays the groundwork, so we can easily move away from the current bundler if we wanted to.
    
    The logging is temporary to measure performance and for debugging purposes.

--- scripts/clean.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-const EXTENSION_DIR = 'dist/extension';
-
-if (fs.existsSync(EXTENSION_DIR)) {
-    fs.rmSync(EXTENSION_DIR, { recursive: true });
-}
--- userscript.config.js
+++ userscript.config.js
@@ -4,7 +4,7 @@
 // @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen :)
 // @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge :)
 // @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età :)
-// @version         %version%
+// @version         __BUILD_VERSION__
 // @author          Zerody (https://github.com/zerodytrash)
 // @namespace       https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/
 // @supportURL      https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/issues
