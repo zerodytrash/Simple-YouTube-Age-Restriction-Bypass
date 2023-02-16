36b990bc98b0063f07152ff7703453cc5787951c
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2023-02-16 20:25:36 +0100

    [userscript] add custom icon
    Related:
    - #180

--- userscript.config.js
+++ userscript.config.js
@@ -4,14 +4,15 @@
 // @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen :)
 // @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge :)
 // @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età :)
+// @icon            https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/raw/v2.5.4/src/extension/icon/icon_64.png
 // @version         __BUILD_VERSION__
 // @author          Zerody (https://github.com/zerodytrash)
 // @namespace       https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/
 // @supportURL      https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/issues
 // @license         MIT
 // @match           https://www.youtube.com/*
-// @match           https://m.youtube.com/*
 // @match           https://www.youtube-nocookie.com/*
+// @match           https://m.youtube.com/*
 // @match           https://music.youtube.com/*
 // @grant           none
 // @run-at          document-start
