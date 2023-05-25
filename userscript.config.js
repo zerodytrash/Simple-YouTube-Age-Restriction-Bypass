cabf7c7fd33146ce18736f4c511be436abe122a5
drunkwinter <38593134+drunkwinter@users.noreply.github.com>
2023-05-25 02:18:19 +0200

    [userscript] add emoji in description
    😎

--- userscript.config.js
+++ userscript.config.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            Simple YouTube Age Restriction Bypass
-// @description     Watch age restricted videos on YouTube without login and without age verification :)
-// @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen :)
-// @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge :)
-// @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età :)
+// @description     Watch age restricted videos on YouTube without login and without age verification 😎
+// @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen 😎
+// @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge 😎
+// @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età 😎
 // @icon            https://github.com/zerodytrash/Simple-YouTube-Age-Restriction-Bypass/raw/v2.5.4/src/extension/icon/icon_64.png
 // @version         __BUILD_VERSION__
 // @author          Zerody (https://github.com/zerodytrash)
